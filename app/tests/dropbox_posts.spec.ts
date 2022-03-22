d02ac471d29f843bf071f676d9747b4f9b4db3fc
TKNGUE <takegue@gmail.com>
2022-03-23 05:55:11 +0900

    [ADD] introduced test tools: vitest

---
+++ app/tests/dropbox_posts.spec.ts
@@ -0,0 +1,21 @@
+import { assert, describe, it } from 'vitest'
+
+import { getDropboxPaperDocuments, getDropboxPaperPost} from '../dropbox_posts';
+
+
+describe('Tutorial vitest', () => {
+  it('Always wrong', async () => {
+    assert.Equal(1, 0)
+  });
+});
+// Test Runnning
+describe('Sample.ts Functions TestCases', () => {
+  it('should return the squared value', async () => {
+    const docs = await getDropboxPaperDocuments();
+    console.dir(docs, {depth: null});
+
+    const doc_id = docs[0].id;
+    const doc = await getDropboxPaperPost(doc_id);
+    console.log(doc, {depth: null});
+  });
+});
